b2da1757a175a08b7fe61b99a453438f2793397d
StarayYaga <45726699+mratom21@users.noreply.github.com>
2022-08-22 17:19:06 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const WebSocket = require('ws');
-const wsServer = new WebSocket.Server({port: 3000});
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+  
+  if (req.url === "/"){
+    fs.readFile(path.join(__dirname, 'client', 'index.html'), (err, data)=>{
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+
+      res.end(data)
+    })
+  }
+}
+)
+
+const wsServer = new WebSocket.WebSocketServer({ server });
 
 wsServer.on('connection', function (ws) {
-    ws.on('message', function message(data, isBinary) {
-        wsServer.clients.forEach(function each(client) {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(data, { binary: isBinary });
-          }
-        });
+    
+  ws.on('message', function message(data, isBinary) {
+      wsServer.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          let json = JSON.parse(data.toString())
+          const message = `{"nicknameIs": "${json.nicknameIs}", "messageIs": "${json.messageIs}"}`
+          client.send(message, { binary: isBinary })
+        }
+      });  
     });
 })
+
+server.listen(8080);
